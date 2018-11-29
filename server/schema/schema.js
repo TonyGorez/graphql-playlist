77dc71aa1dfdc3455c657ebe41a927e2bec4f93f
Tony Gorez <gorez.tony@gmail.com>
2018-11-29 22:00:58 +0100

    delete package lock and add schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,22 +2,40 @@ const graphql = require('graphql')
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
 } = graphql
 const _ = require('lodash');
 
 const fakeBooks = [
-    { name: 'Data Structures', id: '1'},
-    { name: 'Algorithms', id: '2'},
-    { name: 'ReactJs', id: '3'}
+    { name: 'Data Structures', id: '1', genre: 'Computer Science'},
+    { name: 'Algorithms', id: '2', genre: 'Computer Science'},
+    { name: 'ReactJs', id: '3', genre: 'Computer Science'},
+    { name: 'Angular', id: '4', genre: 'Computer Science'}
+]
+
+const fakeAuhtors = [
+    { name: 'Eric Elliot', age: 54, id: '1'},
+    { name: 'Dan Abramov', age: 12, id: '2'},
+    { name: 'Bill Gates', age: 35, id: '3'}
 ]
 
 const BookType = new GraphQLObjectType({
     name: "Book",
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLInt }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID},
+        name: { type: GraphQLString},
+        age: { type: GraphQLString}
     })
 })
 
@@ -26,9 +44,16 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return _.find(fakeBooks, { id: args.id })
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID }},
+            resolve(parent, args) {
+                return _.find(fakeAuhtors, { id: args.id})
             }
         }
     }
